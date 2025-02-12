269e565babc0f61d98f19aebabcb62617e06241e
luki120 <luki120@exerra.xyz>
2025-02-12 20:52:55 -0300

    added more padding modifiers

--- Sources/Brooklyn/Modifiers/Padding.swift
+++ Sources/Brooklyn/Modifiers/Padding.swift
@@ -11,4 +11,20 @@ public extension Stylable {
 	func padding(_ length: CSSDimension = .px(20)) -> Self {
 		self.style("padding: \(length.rawValue)")
 	}
+
+	func paddingTop(_ length: CSSDimension = .px(20)) -> Self {
+		self.style("padding-top: \(length.rawValue)")
+	}
+
+	func paddingRight(_ length: CSSDimension = .px(20)) -> Self {
+		self.style("padding-right: \(length.rawValue)")
+	}
+
+	func paddingBottom(_ length: CSSDimension = .px(20)) -> Self {
+		self.style("padding-bottom: \(length.rawValue)")
+	}
+
+	func paddingLeft(_ length: CSSDimension = .px(20)) -> Self {
+		self.style("padding-left: \(length.rawValue)")
+	}
 }
