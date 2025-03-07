dc2f15ab5cd38893784f144d380890b2be866b6a
luki120 <luki120@exerra.xyz>
2025-03-07 03:50:13 -0300

    added the `center` glue modifier

---
+++ Sources/Brooklyn/Modifiers/Glue/Center.swift
@@ -0,0 +1,14 @@
+//
+//  Center.swift
+//  Brooklyn
+//
+//  Created by Luki on 07/03/2025.
+//
+
+public extension Stylable {
+	func center() -> Self {
+		self
+			.alignItems(.center)
+			.justifyContent(.center)
+	}
+}
