fc853143deb2d5db4c666823bd573cdd11c9f62b
luki120 <luki120@exerra.xyz>
2025-08-26 05:43:01 -0300

    fixed copying fonts • this worked on xcode + cli, the CI was being weird, idfk

--- Sources/Brooklyn/Core/Managers/WebsiteManager.swift
+++ Sources/Brooklyn/Core/Managers/WebsiteManager.swift
@@ -23,16 +23,18 @@ final class WebsiteManager {
 		instance = WebsiteManager(file: file)
 		instance?.cleanBuildDirectory()
 		instance?.copyAssets()
+		instance?.copyFonts()
 	}
 
 	// MARK: - Private
 
-	private let assetsDirectory, buildDirectory: URL
+	private let assetsDirectory, buildDirectory, fontsDirectory: URL
 
 	private init(file: StaticString) {
 		let rootDir: URL = .findProjectRoot(from: file)
 		assetsDirectory = rootDir.appending(path: "Assets/")
 		buildDirectory = rootDir.appending(path: "Build/")
+		fontsDirectory = rootDir.appending(path: "Fonts/")
 	}
 
 	private func cleanBuildDirectory() {
@@ -46,12 +48,35 @@ final class WebsiteManager {
 	}
 
 	private func copyAssets() {
+		guard FileManager.default.fileExists(atPath: assetsDirectory.path(percentEncoded: false)) else { return }
+
 		do {
 			let assets = try FileManager.default.contentsOfDirectory(at: assetsDirectory, includingPropertiesForKeys: nil)
 			try assets.forEach {
 				try FileManager.default.copyItem(
 					at: assetsDirectory.appending(path: $0.lastPathComponent),
 					to: buildDirectory.appending(path: $0.lastPathComponent)
+				)
+			}
+		}
+		catch {
+			print(error.localizedDescription)
+		}
+	}
+
+	private func copyFonts() {
+		guard FileManager.default.fileExists(atPath: fontsDirectory.path(percentEncoded: false)) else { return }
+
+		do {
+			let fonts = try FileManager.default.contentsOfDirectory(at: fontsDirectory, includingPropertiesForKeys: nil)
+
+			let buildFontsDir = buildDirectory.appending(path: "Fonts/")
+			try FileManager.default.createDirectory(at: buildFontsDir, withIntermediateDirectories: true)
+
+			try fonts.forEach {
+				try FileManager.default.copyItem(
+					at: fontsDirectory.appending(path: $0.lastPathComponent),
+					to: buildFontsDir.appending(path: $0.lastPathComponent)
 				)
 			}
 		}
