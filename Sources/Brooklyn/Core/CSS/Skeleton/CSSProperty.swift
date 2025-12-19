15555e7968539aa0e01eb6defb3a01bf3a8065bc
luki120 <luki120@exerra.xyz>
2025-12-19 03:13:01 -0300

    refactored `CSSProperty`, better now

--- Sources/Brooklyn/Core/CSS/Skeleton/CSSProperty.swift
+++ Sources/Brooklyn/Core/CSS/Skeleton/CSSProperty.swift
@@ -9,30 +9,29 @@ import Foundation
 
 /// A struct that represents a CSS property with its respective value, e.g. `display: flex;`
 public struct CSSProperty {
-	let name: String
-	let value: String
+	public let name, value: String
 
 	/// Designated initializer
 	/// - Parameters:
 	///		- name: A `Key` object that represents the property's name
-	///		- value: A string that represents the property's value
+	///		- value: A `RawRepresentable` object that represents the property's value
+	public init<R: RawRepresentable>(name: Key, value: R) where R.RawValue == String {
+		self.name = name.rawValue
+		self.value = value.rawValue
+	}
+
+	/// Designated initializer
+	/// - Parameters:
+	///		- name: A `Key` object that represents the property's name
+	///		- value: A `String` that represents the property's value
 	public init(name: Key, value: String) {
 		self.name = name.rawValue
 		self.value = value
 	}
 
-	/// Designated initializer
-	/// - Parameters:
-	///		- name: A string that represents the property's name
-	///		- value: A string that represents the property's value
-	public init(name: String, value: String) {
-		self.name = name
-		self.value = value
-	}
-
 	/// A custom initializer that takes a `CSSAnimation` object
 	/// - Parameters:
-	///		- animation: A `CSSAnimation` object that represents a CSS animation
+	///   - animation: A `CSSAnimation` object that represents a CSS animation
 	public init(animation: CSSAnimation) {
 		self.name = "animation"
 		self.value = animation.description
@@ -40,29 +39,42 @@ public struct CSSProperty {
 }
 
 public extension CSSProperty {
-	/// An enum that represents a CSS property's name
-	@frozen enum Key: String {
-		case alignItems = "align-items"
-		case backgroundColor = "background-color"
-		case borderRadius = "border-radius"
-		case bottom
-		case color
-		case content
-		case display
-		case flexDirection = "flex-direction"
-		case fontFamily = "font-family"
-		case fontSize = "font-size"
-		case fontWeight = "font-weight"
-		case height
-		case justifyContent = "justify-content"
-		case left
-		case margin
-		case padding
-		case position
-		case textAlign = "text-align"
-		case textDecoration = "text-decoration"
-		case transform
-		case transition
-		case width
+	/// A struct that represents a CSS property's name
+	struct Key: ExpressibleByStringLiteral, RawRepresentable {
+		public let rawValue: String
+
+		public init(rawValue: String) {
+			self.rawValue = rawValue
+		}
+
+		public init(stringLiteral value: String) {
+			self.rawValue = value
+		}
 	}
+}
+
+@MainActor
+public extension CSSProperty.Key {
+	static let alignItems: Self = "align-items",
+			   backgroundColor: Self = "background-color",
+			   borderRadius: Self = "border-radius",
+			   bottom: Self = "bottom",
+			   color: Self = "color",
+			   content: Self = "content",
+			   display: Self = "display",
+			   flexDirection: Self = "flex-direction",
+			   fontFamily: Self = "font-family",
+			   fontSize: Self = "font-size",
+			   fontWeight: Self = "font-weight",
+			   height: Self = "height",
+			   justifyContent: Self = "justify-content",
+			   left: Self = "left",
+			   margin: Self = "margin",
+			   padding: Self = "padding",
+			   position: Self = "position",
+			   textAlign: Self = "text-align",
+			   textDecoration: Self = "text-decoration",
+			   transform: Self = "transform",
+			   transition: Self = "transition",
+			   width: Self = "width"
 }
